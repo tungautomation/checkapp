ecaeed0a2c2e9752e21f889ed24ef161de1be500
tungautomation <bkv253@gmail.com>
2024-02-19 12:44:23 +0700

    update

--- flutter_application_1/lib/information_setting.dart
+++ flutter_application_1/lib/information_setting.dart
@@ -1,10 +1,12 @@
 
 import 'package:flutter/material.dart';
+// import 'dart:convert';
+
 
 void main() {
   runApp(const Setting());
 }
-
+String option = "one";
 class Setting extends StatelessWidget {
   const Setting({super.key});
 
@@ -16,7 +18,6 @@ class Setting extends StatelessWidget {
     );
   }
 }
-
 class information extends StatefulWidget {
   const information({super.key});
   @override
@@ -25,17 +26,78 @@ class information extends StatefulWidget {
 
 class _informationState extends State<information> {
   @override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Cập nhật thông tin:-"),),
+      appBar: AppBar(title: const Center(child: Text("Cập nhật thông tin")),),
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.only(top:28),
+              child: Center(
+                child: Container(
+                  width: 240,
+                  height: 240,
+                  decoration:const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(60)),color: Colors.amber),
+                  child: Stack(
+                    children: [
+                      Center(child: Image.asset('images/477290.png')),
+                      Positioned(bottom: 6,right: 6,
+                      child: Container(
+                        decoration:const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(100)),color: Colors.black12),
+                        child: IconButton(onPressed: ()
+                        {
+                          setState(() {
+                            
+                          });
+                        },
+                         icon:  const Icon(Icons.add,color: Colors.red,size: 40,),
+                         
+                         ),
+                         ),
+                         ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Table(
+                // border: TableBorder.all(),
+                children: const [
+                  TableRow(
+                    children: [Padding(
+                      padding: EdgeInsets.only(right: 10 ),
+                      child: TableCell(child: TextField(decoration: InputDecoration(labelText: "Họ:*"),)),
+                    ),TableCell(child: TextField(decoration: InputDecoration(labelText: "Tên:*"),))]
+                  )
+                ],
+              ),
+            ),
+            Table(
+              children: [
+                Input_update("Số CCCD / CMTND"),
+                Input_update("Email:*"),
+                Input_update('Số điện thoại:*'),
+              ],
+            ),
 
           ],
         ),
       ),
-
     );
+  }
+
+  TableRow Input_update(String labelname) {
+    return  TableRow(children: [Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: TableCell(child: TextField(decoration: InputDecoration(labelText: labelname),)),
+            ),
+            ],
+            );
   }
 }
---
+++ flutter_application_1/lib/province.dart
@@ -0,0 +1,10 @@
+// import 'dart:convert';
+// import 'dart:core';
+
+// import 'package:flutter_application_1/province-data.dart';
+
+// List<Province> parsePhoto(String responseBody)
+// {
+//   var List = jsonDecode(responseBody) as List<dynamic>;
+//   List<Photo> photos
+// }
